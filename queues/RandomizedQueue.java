e67ab171f76efcf38534900fc415251202bfab9c
Ervin Wu <ecwu@live.unc.edu>
2017-05-20 13:39:10 -0400

    fixed folder name

--- queues/RandomizedQueue.java
+++ queues/RandomizedQueue.java
@@ -1,4 +1,3 @@
-package Deque;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
--- queues/dequeTest.java
+++ queues/dequeTest.java
@@ -1,10 +1,6 @@
-package Deque;
-
 import static org.junit.Assert.*;
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
