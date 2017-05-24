104e79cd075395c2275eb9a85fec52bae3ab6b51
Ervin Wu <ervin.c.wu@gmail.com>
2017-05-24 14:27:06 -0400

    fix unit test

--- queues/dequeTest.java
+++ queues/dequeTest.java
@@ -1,77 +1,62 @@
+
+
 import static org.junit.Assert.*;
-import java.util.Iterator;
+
 import java.util.NoSuchElementException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class dequeTest {
-	private Deque<Comparable> deque = new Deque();
+public class RandomizedQueueTest {
+	private RandomizedQueue qr;
+	@Before
+	public void setUp() throws Exception {
+		qr = new RandomizedQueue();
+	}
 
-	@Test
-	public void addFirstTEST() {
-		deque.addFirst("test");
-		assertEquals(1, deque.size());
-		deque.addFirst(12);
-		assertEquals(2, deque.size());
+	@After
+	public void tearDown() throws Exception {
 	}
 
 	@Test
-	public void addLastTEST() {
-		deque.addLast("test");
-		assertEquals(1, deque.size());
-		deque.addLast(12);
-		assertEquals(2, deque.size());
+	public void testEnqueue() {
+		qr.enqueue(123);
+		assertEquals(123,qr.dequeue());
 	}
 
 	@Test
-	public void RemoveLaastTEST() {
-		deque.addLast("test");
-		deque.addLast(12);
-
-		Object item = deque.removeLast();
-		assertEquals(12, item);
-
-		item = deque.removeLast();
-		assertEquals("test", item);
+	public void testEnqueueEmpty() {
+		qr.enqueue(123);
+		qr.dequeue();
+		qr.enqueue(1233);
+		assertEquals(1233,qr.dequeue());
+		assertEquals(true, qr.isEmpty());
 	}
-
+	@SuppressWarnings("unchecked")
 	@Test
-	public void RemoveFirstTEST() {
-		deque.addFirst("test");
-		assertEquals(1, deque.size());
-		deque.addFirst(12);
-		assertEquals(2, deque.size());
-		Object item = deque.removeFirst();
-		assertEquals(12, item);
-		item = deque.removeFirst();
-		assertEquals("test", item);
+	public void testEnqueueCount() {
+		qr.enqueue(123);
+		qr.enqueue(1233);
+		qr.enqueue(1233);
+		qr.enqueue(1233);
+		qr.enqueue(1233);
+		qr.enqueue(1233);
+		assertEquals(6, qr.size());
 	}
-
 	@Test(expected = NoSuchElementException.class)
-	public void RemoveFirstTESTNull() {
-		Object item = deque.removeFirst();
-		assertEquals(null, item);
+	public void testfailEnqueue() {
+		qr.enqueue(123);
+		qr.dequeue();
+		qr.dequeue();
+		qr.dequeue();
+
+	}
+	@Test
+	public void testIsEmpty() {
+		qr.isEmpty();
 	}
 
-	@Test(expected = NoSuchElementException.class)
-	public void removeLastTESTNull() {
-		Object item = deque.removeLast();
-		assertEquals(null, item);
-	}
-	
-	@Test
-	public void itertest(){
-		deque.addFirst("test1");
-		deque.addFirst("test2");
-		deque.addFirst("test3");
-		deque.addFirst("test4");
-		
-		Iterator<Comparable> comparable = deque.iterator();
-			 
-			  while(comparable.hasNext()){
-				  System.out.println(comparable.next());
-			  }
-		
-	}
+
+
 }
